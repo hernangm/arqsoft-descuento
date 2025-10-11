0249fc96ba38a68d05ba5dd080cacc970b10371e
Hernan Marano <herchugm@gmail.com>
2025-10-11 12:54:27 -0300

    subir cambios

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from descuentos import aplicarDescuento
+from impuestos import calcularImpuesto
 # INPUT
 # { "items": [
 #   { "id": 1, "product_category": "category_a", "price": 10.0 },
@@ -22,15 +23,13 @@ def facturar(ticket):
     descs = []
     for i in ticket["items"]:
         descs.append(aplicarDescuento(ticket["discount"], i["price"]))
-
+        tax = calcularImpuesto(i["product_category"], i["price"] - descs[-1])
+        item_taxes.append({"id": i["id"], "tax": tax})
 
     return {
         "subtotal": 0.0,
         "applied_discount": sum(descs),
-        "item_taxes": [
-            { "id": 1, "tax": 0.0 },
-            { "id": 2, "tax": 0.0 }
-        ],
-        "total_taxes": 0.0,
-        "final_total": 0.0
+        "item_taxes": item_taxes,
+        "total_taxes": sum([it["tax"] for it in item_taxes]),
+        "final_total": sum([i["price"] for i in ticket["items"]]) - sum(descs) + sum([it["tax"] for it in item_taxes])
     }
